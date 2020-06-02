595485cdd54761cc6a1aa80e3f3f83a164d45dca
ellensdogos <ellen.sdogos@hotmail.com>
2020-06-02 23:42:52 +0200

    frontend almonst done

---
+++ Backend/public/javascripts/admin.js
@@ -0,0 +1,94 @@
+var adminName = "test";
+var adminPassword = "1234";
+
+var page = document.getElementById("mainPage");
+
+console.log(localStorage);
+MainPage();
+
+function MainPage() {
+    if (localStorage.getItem("admin") == adminPassword) {
+        AdminPage();
+    }
+    else {
+        LoginPage();
+    }
+}
+
+function LoginPage() {
+    page.innerHTML = "";
+    page.insertAdjacentHTML("beforeend", "<div><h2>Logga in som admin</h2></div><div><form><div><input class='login' type='text' id='username' placeholder='Användarnamn'></div></br><div><input class='login' type='password' id='password' placeholder='Lösenord'></div></form><button id='loginBtn' type='button' class='loginBtn'>Logga in</button></div>");
+
+    let loginButton = document.getElementById("loginBtn");
+
+    loginButton.addEventListener("click", function () {
+        let name = document.getElementById("username").value;
+        let password = document.getElementById("password").value;
+
+        if (name == adminName && password == adminPassword) {
+            localStorage.setItem(adminName, adminPassword)
+            console.log("Inloggad");
+            AdminPage();
+
+        }
+        else {
+            console.log("Misslyckad inloggning");
+            page.innerHTML = "";
+            page.insertAdjacentHTML("beforeend", "<div><h3>Inloggning misslyckades!</h3><button id='loginBtn' onclick='LoginPage()'>Försök igen</button></div>")
+        }
+    });
+}
+
+function AdminPage() {
+    page.innerHTML = "";
+    page.insertAdjacentHTML("beforeend",
+        `
+    <div><h1 class="header">Administration</h1><div>
+    <div class="container">
+        <div class="column"><button id="btn-standard" onclick="showAllUsers()"> Visa alla användare </button></div>
+        <div class="column"><button id="btn-standard" onclick="showAllSubscribers()"> Visa alla prenumeranter </button></div>
+        <div class="column"><button id="btn-standard" onclick="logout()"> Logga ut </button></div>
+    </div>
+
+    `);
+}
+
+function showAllUsers() {
+    fetch('http://localhost:3000/users')
+        .then(res => res.json())
+        .then(function (json) {
+            page.innerHTML = "";
+            page.insertAdjacentHTML("beforeend", "<div><h3>Alla användare:</h3></div>");
+
+            for (i = 0; i < json.length; i++) {
+                page.insertAdjacentHTML("beforeend", "<ul><li> Namn: " + json[i].userName + "</li><li> Email: " + json[i].userEmail + "</li><li> Prenumerant: " + json[i].subscribe + "</li></ul>");
+            }
+
+            page.insertAdjacentHTML("beforeend", "<div><button id='goBackButton' onclick='AdminPage()'> Gå tillbaka </button></div>");
+        })
+}
+
+function showAllSubscribers() {
+    fetch('http://localhost:3000/users')
+        .then(res => res.json())
+        .then(function (json) {
+            page.innerHTML = "";
+            page.insertAdjacentHTML("beforeend", "<div><h3>Prenumererar på nyhetsbrevet:</h3></div>");
+            
+            let allSubscribers = "";
+            var subscribers = json.filter(a => a.subscribe == true);
+
+            for (i = 0; i < subscribers.length; i++) {
+                allSubscribers += subscribers[i].userEmail + ", ";
+            }
+
+            page.insertAdjacentHTML("beforeend", "<div>" + allSubscribers + "</div>");
+            page.insertAdjacentHTML("beforeend", "<div><button id='goBackButton' onclick='AdminPage()'> Gå tillbaka </button></div>");
+        })
+}
+
+function logout() {
+    localStorage.clear();
+    MainPage();
+}
+
--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,9 +1,44 @@
 var express = require('express');
+var fs = require('fs');
+
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  fs.readFile('users.json', (err, data) => {
+    if (err) throw err;
+
+    var users = JSON.parse(data);
+    res.send(users);
+  })
+
+});
+
+router.post('/', function(req, res, next) {
+  fs.readFile('users.json', (err, data) => {
+    if (err) throw err;
+
+    var users = JSON.parse(data);
+
+    newUser = {
+      "id": req.body.id,
+      "userName": req.body.userName,
+      "userEmail": req.body.userEmail,
+      "password": req.body.password,
+      "subscribe": req.body.subscribe
+    }
+
+    users.push(newUser);
+
+    var saveUsers = JSON.stringify(users, null, 2);
+
+    fs.writeFile('users.json', saveUsers, (err, data) => {
+      if (err) throw err;
+    })
+
+    res.send('Ny användare sparad');
+  })
+
 });
 
 module.exports = router;
